af5569ff4d86f209349e5e4150cafa11467ef67e
omerwyo <omerbaggia123@gmail.com>
2022-03-26 16:21:18 +0800

    Demo materials

---
+++ demo.py
@@ -0,0 +1,26 @@
+from impl import BloomFilter
+import sys
+
+def load_usernames(filename, holder):
+    f = open(filename, 'r')
+    line = f.readline()
+    while line:
+        line = line.strip()
+        holder.add(line)
+        line = f.readline()
+    return holder
+
+
+if __name__ == '__main__':
+    bloomfilter = BloomFilter(specify_item_count=True, param=100000, fp_prob=0.05)
+
+    # A dataset of 160,000 Twitter usernames
+    filename = 'input.txt'
+
+    bloomfilter = load_usernames(filename, bloomfilter)
+    holder = load_usernames(filename, set())
+
+    print(f'Size of set {sys.getsizeof(holder) * 8}') # sys get size returns the number of bytes an object takes up, so we divide by 8
+    print(f'Size of bloomfilter {bloomfilter.size}') 
+
+    
