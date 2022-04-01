51b39731cefd515411ffba7ba2f9ab28fc5c9d60
omerwyo <omerbaggia123@gmail.com>
2022-04-01 12:28:50 +0800

    Changes to Demo

--- demo.py
+++ demo.py
@@ -12,7 +12,7 @@ def load_usernames(filename, holder):
 
 
 if __name__ == '__main__':
-    bloomfilter = BloomFilter(specify_item_count=True, param=160000, fp_prob=0.05)
+    bloomfilter = BloomFilter(specify_item_count=True, param=160000, fp_prob=1/100000)
 
     # A dataset of 160,000 Twitter usernames
     filename = 'input.txt'
@@ -20,8 +20,8 @@ if __name__ == '__main__':
     bloomfilter = load_usernames(filename, bloomfilter)
     holder = load_usernames(filename, set())
 
-    print(f'Size of set {sys.getsizeof(holder) * 8}') # sys get size returns the number of bytes an object takes up, so we divide by 8
-    print(f'Size of bloomfilter {bloomfilter.size}') 
-    print(f'ratio of bloomfilter vs set 1 : {sys.getsizeof(holder) * 8 / bloomfilter.size}')
+    print(f'Size of Set: {sys.getsizeof(holder) * 8} bits') # sys get size returns the number of bytes an object takes up, so we divide by 8
+    print(f'Size of BloomFilter {bloomfilter.size} bits') 
+    print(f'The Set takes up {(sys.getsizeof(holder) * 8 / bloomfilter.size):.3f} times more space than the BloomFilter')
 
     
